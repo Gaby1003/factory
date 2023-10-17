7f709ca5cef7da4ca2c277542dcd7e4b3cafe57a
unknown <juan.figueroa01@uptc.edu.co>
2023-10-16 23:12:30 -0500

    fix sql script conflict

---
+++ src/main/java/com/uptc/frw/factory/jpa/entity/Machine.java
@@ -0,0 +1,70 @@
+package com.uptc.frw.factory.jpa.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "MAQUINAS")
+public class Machine {
+
+    @Id
+    @Column(name = "NUMERO_SERIE")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String serialNumber;
+
+    @Column(name = "MARCA")
+    private String brand;
+
+    @Column(name = "MODELO")
+    private String model;
+
+    @Column(name = "FECHA_COMPRA")
+    private Date purchaseDate;
+
+    @OneToMany(mappedBy = "machine")
+    private List<MachineHistory> machineHistories;
+
+    public Machine() {
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Date getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(Date purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public List<MachineHistory> getMachineHistories() {
+        return machineHistories;
+    }
+
+    public void setMachineHistories(List<MachineHistory> machineHistories) {
+        this.machineHistories = machineHistories;
+    }
+}
---
+++ src/main/java/com/uptc/frw/factory/jpa/entity/MachineHistory.java
@@ -0,0 +1,64 @@
+package com.uptc.frw.factory.jpa.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "HISTORICO_MAQUINAS")
+@IdClass()
+public class MachineHistory {
+
+    @Id
+    @Column(name = "ID_TRABAJADOR")
+    private Long workerId;
+
+    @Id
+    @Column(name = "ID_MAQUINA")
+    private Long machineId;
+
+    @Id
+    @Column(name = "FECHA")
+    private Long date;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_MAQUINA")
+    private Machine machine;
+
+//    @ManyToOne
+//    @JoinColumn(name = "ID_TRABAJADOR")
+//    private Worker worker;
+
+    public MachineHistory() {
+    }
+
+    public Long getWorkerId() {
+        return workerId;
+    }
+
+    public void setWorkerId(Long workerId) {
+        this.workerId = workerId;
+    }
+
+    public Long getMachineId() {
+        return machineId;
+    }
+
+    public void setMachineId(Long machineId) {
+        this.machineId = machineId;
+    }
+
+    public Long getDate() {
+        return date;
+    }
+
+    public void setDate(Long date) {
+        this.date = date;
+    }
+
+    public Machine getMachine() {
+        return machine;
+    }
+
+    public void setMachine(Machine machine) {
+        this.machine = machine;
+    }
+}
