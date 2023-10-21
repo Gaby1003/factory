e373f469eb36ac33bfcedea40681a2d2b359c497
Juan Nicolas Figueroa <juan.figueroa01@uptc.edu.co>
2023-10-20 21:38:55 -0500

    documents

--- src/main/java/com/uptc/frw/factory/jpa/repository/LogRepository.java
+++ src/main/java/com/uptc/frw/factory/jpa/repository/LogRepository.java
@@ -3,12 +3,6 @@ package com.uptc.frw.factory.jpa.repository;
 import com.uptc.frw.factory.jpa.entity.Log;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
-import java.util.List;
-
 public interface LogRepository extends ElasticsearchRepository<Log, String> {
-
-    List<Log> findByAction(String action);
-
-    List<Log> findByDescriptionContaining(String name);
 
 }
